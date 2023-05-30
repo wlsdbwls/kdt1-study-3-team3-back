aec47a9e8b916268cbc54bac64e55a7871f96931
jeongdawun <jeongdawun33@gmail.com>
2023-05-30 21:34:52 +0900

    fix: [정다운] import 수정

--- src/main/java/kr/eddi/demo/config/CorsConfig.java
+++ src/main/java/kr/eddi/demo/config/CorsConfig.java
@@ -9,8 +9,13 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://127.0.0.1:8080")
-                .allowedOrigins("http://localhost:8080")
+//                .allowedOrigins("http://127.0.0.1:8080")
+//                .allowedOrigins("http://localhost:8080")
+                .allowedOrigins(
+                        "http://127.0.0.1:8080",
+                        "http://localhost:8080",
+                        "http://3.38.61.219", // 다운
+                        "http://ec2-3-38-61-219.ap-northeast-2.compute.amazonaws.com/")
                 .allowedMethods("GET", "POST", "PUT", "DELETE");
     }
 }
