34902e99b6bf5f1958ac5e6a03d3def61756edb5
wlsdbwls <wlsdbwls614@gmail.com>
2023-06-02 10:36:12 +0900

    fix: [진유진] 배포를 위한 Corsconfig 수정 [FIX]

--- src/main/java/kr/eddi/demo/config/CorsConfig.java
+++ src/main/java/kr/eddi/demo/config/CorsConfig.java
@@ -11,6 +11,7 @@ public class CorsConfig implements WebMvcConfigurer {
         registry.addMapping("/**")
                 .allowedOrigins("http://127.0.0.1:8080")
                 .allowedOrigins("http://localhost:8080")
+                .allowedOrigins("http://15.164.237.78")
                 .allowedMethods("GET", "POST", "PUT", "DELETE");
     }
 }
